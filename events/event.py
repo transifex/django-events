23c72965c1393cd09d0c6aa3eac0bd574c4c8da1
Apostolos Bessas <mpessas@transifex.com>
2013-04-10 13:03:23 +0300

    Log an error message on issues with handlers

--- events/event.py
+++ events/event.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 
 class Event(object):
     """Class to reperesent an event."""
@@ -12,8 +14,9 @@ class Event(object):
         self._listeners.append((l, action))
 
     def emit(self, *args, **kwargs):
+        logger = logging.getLogger('events')
         for l, action in self._listeners:
             try:
                 l(action=action)(*args, **kwargs)
             except Exception:
-                pass
+                logger.error("Error calling %s", action, exc_info=True)
